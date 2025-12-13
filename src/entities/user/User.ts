6ea0513a93152d9fa9774dfd42d6b745c51eccb0
NarioMendonca <nario.sousa2014@gmail.com>
2025-12-13 13:25:35 -0300

    feat: create user entity

---
+++ src/entities/user/User.ts
@@ -0,0 +1,52 @@
+import { Email } from "../email/Email.js";
+
+type userParams = {
+  id: string;
+  name: string;
+  email: Email;
+  passwordHash: string;
+  createdAt: string | Date;
+};
+
+export class UserEntity {
+  id: string;
+  name: string;
+  email: Email;
+  passwordHash: string;
+  createdAt: string | Date;
+
+  constructor({ id, name, email, passwordHash, createdAt }: userParams) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.passwordHash = passwordHash;
+    this.createdAt = createdAt;
+  }
+
+  public static create(params: userParams) {
+    const userData = {
+      ...params,
+    };
+    return new UserEntity(userData);
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public getName() {
+    return this.name;
+  }
+
+  public getEmail() {
+    return this.email;
+  }
+
+  public getPasswordHash() {
+    return this.passwordHash;
+  }
+
+  public getCreatedAt() {
+    return this.createdAt;
+  }
+}
