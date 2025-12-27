b56a220aa07cca5a2523e4df20d4bc2040b00b99
NarioMendonca <nario.sousa2014@gmail.com>
2025-12-27 16:42:10 -0300

    feat: implements passwordHash on infra

---
+++ src/infra/user/passwordHash.ts
@@ -0,0 +1,14 @@
+import bcrypt from "bcryptjs";
+import { PasswordHashModel } from "../../entities/user/services/PasswordHashModel.js";
+
+export class PasswordHash implements PasswordHashModel {
+  async hash(password: string): Promise<string> {
+    const passwordHash = await bcrypt.hash(password, 12);
+    return passwordHash;
+  }
+
+  async compare(password: string, hash: string): Promise<boolean> {
+    const passwordMatch = bcrypt.compare(password, hash);
+    return passwordMatch;
+  }
+}
