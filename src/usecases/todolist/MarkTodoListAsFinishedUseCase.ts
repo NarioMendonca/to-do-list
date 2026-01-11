8abf1932f3329429fe0f4c3e1276a0e1386efff7
NarioMendonca <nario.sousa2014@gmail.com>
2026-01-11 17:35:02 -0300

    feat: create markTodoListAsFinishedUseCase

---
+++ src/usecases/todolist/MarkTodoListAsFinishedUseCase.ts
@@ -0,0 +1,27 @@
+import { NotFoundError } from "../../errors/usecases/NotFoundError.js";
+import { TodoListRepository } from "../../repositories/TodoListRepository.js";
+import { UseCase } from "../UseCase.js";
+
+type MarkTodoListAsFinishedInputDTO = {
+  listId: string;
+};
+
+type MarkTodoListAsFinishedOutputDTO = Promise<void>;
+
+export class MarkTodoListAsFinishedUseCase implements UseCase<
+  MarkTodoListAsFinishedInputDTO,
+  MarkTodoListAsFinishedOutputDTO
+> {
+  constructor(private readonly todoListRepository: TodoListRepository) {}
+
+  async handle({
+    listId,
+  }: MarkTodoListAsFinishedInputDTO): Promise<MarkTodoListAsFinishedOutputDTO> {
+    const list = await this.todoListRepository.getList(listId);
+    if (!list) {
+      throw new NotFoundError("List not found");
+    }
+    list.markListAsFinished();
+    await this.todoListRepository.save(list);
+  }
+}
