b24766cb4eb0c70e45859ed9c188502999a35ec5
NarioMendonca <nario.sousa2014@gmail.com>
2026-01-11 19:05:09 -0300

    feat: create markItemAsCompletedUseCase

---
+++ src/usecases/todolist/MarkItemAsCompletedUseCase.ts
@@ -0,0 +1,37 @@
+import { NotFoundError } from "../../errors/usecases/NotFoundError.js";
+import { TodoListRepository } from "../../repositories/TodoListRepository.js";
+import { UseCase } from "../UseCase.js";
+
+type MarkItemAsCompletedInputDTO = {
+  todoListId: string;
+  todoItemId: string;
+};
+
+type MarkItemAsCompletedOutputDTO = void;
+
+export class MarkItemAsCompletedUseCase implements UseCase<
+  MarkItemAsCompletedInputDTO,
+  MarkItemAsCompletedOutputDTO
+> {
+  constructor(private readonly todoListRepository: TodoListRepository) {}
+
+  async handle({
+    todoListId,
+    todoItemId,
+  }: MarkItemAsCompletedInputDTO): Promise<MarkItemAsCompletedOutputDTO> {
+    const todoItem = await this.todoListRepository.getTodoItem({
+      todoListId,
+      todoItemId,
+    });
+    if (!todoItem) {
+      throw new NotFoundError("Todo item not found");
+    }
+    const todoList = await this.todoListRepository.getList(todoListId);
+    if (!todoList) {
+      throw new NotFoundError("Todo item not found");
+    }
+    todoList.markTodoItemAsFinished(todoItem);
+
+    await this.todoListRepository.save(todoList);
+  }
+}
