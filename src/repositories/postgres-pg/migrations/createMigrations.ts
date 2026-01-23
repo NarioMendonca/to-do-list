14aa717b81c0c70cb1f79f6d8545878ac890556a
NarioMendonca <nario.sousa2014@gmail.com>
2026-01-22 23:01:13 -0300

    migration: create todo lists tables

--- src/repositories/postgres-pg/migrations/createMigrations.ts
+++ src/repositories/postgres-pg/migrations/createMigrations.ts
@@ -10,13 +10,81 @@ async function runMigrations() {
   await client.query(
     `CREATE TABLE IF NOT EXISTS users (
       id UUID PRIMARY KEY,
-      name TEXT,
-      email TEXT,
-      is_email_verified boolean,
-      password_hash TEXT,
+      name TEXT NOT NULL,
+      email TEXT NOT NULL,
+      is_email_verified boolean NOT NULL,
+      password_hash TEXT NOT NULL,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
     )`,
   );
+
+  await client.query(
+    `CREATE TABLE IF NOT EXISTS week_days (
+      id SMALLINT PRIMARY KEY,
+      day VARCHAR(9)
+    )`,
+  );
+
+  await client.query(
+    `INSERT INTO week_days (id, day) VALUES
+    (0, 'Sunday'), (1, 'Monday'), (2, 'Tuesday'), (3, 'Wednesday'), (4, 'Thursday'), (5, 'Friday'), (6, 'Saturday')`,
+  );
+
+  await client.query(
+    `CREATE TABLE IF NOT EXISTS todo_lists (
+      id UUID PRIMARY KEY,
+      title TEXT NOT NULL,
+      motivation_phrase TEXT,
+      total_items SMALLINT NOT NULL,
+      days_to_repeat SMALLINT,
+      planned_day_to_make TIMESTAMP WITH TIME ZONE,
+      expiration_dt TIMESTAMP WITH TIME ZONE,
+      finished_dt TIMESTAMP WITH TIME ZONE,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+    )
+    `,
+  );
+
+  await client.query(
+    `CREATE TABLE IF NOT EXISTS days_who_todo_list_repeat (
+      day_id SMALLINT NOT NULL,
+      todo_list_id UUID NOT NULL,
+
+      PRIMARY KEY (day_id, todo_list_id),
+
+      CONSTRAINT fk_week_day FOREIGN KEY(day_id) REFERENCES week_days(id),
+      CONSTRAINT fk_todo_list FOREIGN KEY(todo_list_id) REFERENCES todo_lists(id) ON DELETE CASCADE
+    )
+    `,
+  );
+
+  await client.query(
+    `CREATE TABLE IF NOT EXISTS todo_items (
+      id UUID PRIMARY KEY NOT NULL,
+      title TEXT NOT NULL,
+      description TEXT,
+      is_completed boolean,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+      todo_list_id UUID NOT NULL,
+
+      CONSTRAINT fk_todo_list
+        FOREIGN KEY (todo_list_id)
+        REFERENCES todo_lists(id)
+        ON DELETE CASCADE
+    )
+    `,
+  );
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS todo_items_in_todo_list (
+      todo_list_id UUID NOT NULL,
+      todo_item_id UUID NOT NULL,
+      
+      PRIMARY KEY (todo_list_id, todo_item_id),
+
+      CONSTRAINT fk_todo_list FOREIGN KEY(todo_list_id) REFERENCES todo_lists(id) ON DELETE CASCADE,
+      CONSTRAINT fk_todo_item FOREIGN KEY(todo_item_id) REFERENCES todo_items(id) ON DELETE CASCADE
+    )`);
 
   await client.end();
 }
