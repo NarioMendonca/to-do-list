93cf27fdcc6903cae3418b5fdb1dcc54f2e26dd0
NarioMendonca <nario.sousa2014@gmail.com>
2026-01-25 15:11:32 -0300

    feat: add fetchByUser on todoListReadModelRepository

--- src/repositories/TodoListReadModelRepository.ts
+++ src/repositories/TodoListReadModelRepository.ts
@@ -2,5 +2,5 @@ import { TodoListDTO } from "../model/TodoList.js";
 
 export interface TodoListReadModelRepository {
   get(listId: string): Promise<TodoListDTO | null>;
-  fetch(listId: string[]): Promise<TodoListDTO[]>;
+  fetchByUser(userId: string): Promise<TodoListDTO[]>;
 }
