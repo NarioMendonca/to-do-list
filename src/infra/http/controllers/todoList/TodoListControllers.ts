27e18f747d2b50251cda3a64f92d9afdbd5afdcb
NarioMendonca <nario.sousa2014@gmail.com>
2026-02-03 13:51:41 -0300

    feat: make create todo list controller

---
+++ src/infra/http/controllers/todoList/TodoListControllers.ts
@@ -0,0 +1,12 @@
+import { makeCreateTodoListUseCase } from "../../../../usecases/todolist/factories/makeCreateTodoListUseCase.js";
+import { Req, Res } from "../../server.js";
+import { CreateTodoListController } from "./CreateTodoListController.js";
+
+export class TodoListControllers {
+  public create = async (req: Req, res: Res) => {
+    const todoListController = new CreateTodoListController(
+      makeCreateTodoListUseCase(),
+    );
+    return await todoListController.handle(req, res);
+  };
+}
