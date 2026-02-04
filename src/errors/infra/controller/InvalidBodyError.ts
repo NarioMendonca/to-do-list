049a838355024c9f78c89230cd9495b109044d7e
NarioMendonca <nario.sousa2014@gmail.com>
2026-02-04 18:54:05 -0300

    fix: add statusCode param to erros who extends controller

--- src/errors/infra/controller/InvalidBodyError.ts
+++ src/errors/infra/controller/InvalidBodyError.ts
@@ -1,7 +1,7 @@
 import { ControllerError } from "./ControllerError.js";
 
 export class InvalidBodyError extends ControllerError {
-  constructor(message: string = "Invalid body") {
-    super(message);
+  constructor(message: string = "Invalid body", statusCode: number = 400) {
+    super(message, statusCode);
   }
 }
