1229f9aa8aad4fb17956b0a3430902399eed4432
NarioMendonca <nario.sousa2014@gmail.com>
2026-02-06 15:42:15 -0300

    feat:  create todoListRead repository

--- src/repositories/TodoListReadRepository.ts
+++ src/repositories/TodoListReadRepository.ts
@@ -1,6 +1,6 @@
 import { TodoListDTO } from "../model/TodoList.js";
 
-export interface TodoListReadModelRepository {
+export interface TodoListReadRepository {
   get(listId: string): Promise<TodoListDTO | null>;
   fetchByUser(userId: string): Promise<TodoListDTO[]>;
 }
