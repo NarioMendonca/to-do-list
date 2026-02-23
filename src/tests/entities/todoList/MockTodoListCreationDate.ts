bcf505a6089a7924ffd8edc2a2daa2b158b1e8ec
NarioMendonca <nario.sousa2014@gmail.com>
2026-02-23 16:35:30 -0300

    feat: create mockDaysWeekToRepeat function

--- src/tests/entities/todoList/MockTodoListCreationDate.ts
+++ src/tests/entities/todoList/MockTodoListCreationDate.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { TodoListParams } from "../../../entities/todoList/TodoList.js";
+import { mockDaysWeekToRepeat } from "./MockDaysWeekToRepeat.js";
 
 export function createMockTodoList() {
   const todoListExpirationDt = faker.date.soon();
@@ -8,7 +9,7 @@ export function createMockTodoList() {
     id: faker.string.uuid(),
     title: faker.word.words({ count: { min: 5, max: 10 } }),
     createdAt: new Date(),
-    daysWeekToRepeat: [0, 1, 2, 3, 6],
+    daysWeekToRepeat: mockDaysWeekToRepeat(),
     expirationDt: todoListExpirationDt,
     finishedDt: faker.date.soon({ refDate: todoListExpirationDt }),
     plannedDtToMake: faker.date.soon({ refDate: todoListExpirationDt }),
