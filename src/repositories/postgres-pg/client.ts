515d650192aabb311552603539e30ba1e65b3a00
NarioMendonca <nario.sousa2014@gmail.com>
2026-02-25 12:37:06 -0300

    fix: replace db singleton for a pg pool instead of pg client

--- src/repositories/postgres-pg/client.ts
+++ src/repositories/postgres-pg/client.ts
@@ -1,10 +1,14 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 import env from "../../infra/env/getEnvs.js";
 import { DatabaseConnectionError } from "../../errors/infra/DatabaseConnectionError.js";
 
-const db = new Client({
+const MAX_POOLS_COUNT = 16;
+
+const db = new Pool({
   connectionString:
     env.NODE_ENV !== "test" ? env.DATABASE_URL : env.TEST_DATABASE_URL,
+  max: MAX_POOLS_COUNT,
+  min: MAX_POOLS_COUNT / 2,
 });
 try {
   await db.connect();
